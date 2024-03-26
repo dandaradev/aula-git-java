fcad536f4385120c2591fbbf583930d0d185b12f
dandaradev <dandara.vic1@gmail.com>
2024-03-26 10:26:40 -0300

    descrição

--- aula/src/main/java/aula/git/Apresentacao.java
+++ aula/src/main/java/aula/git/Apresentacao.java
@@ -4,10 +4,17 @@ public class Apresentacao {
 
     public void apresentar() {
         this.lucasSouza();
+        this.dandara();
     }
 
     private void lucasSouza() {
         System.out.println("");
         System.out.println("Estou colaborando com o git");
     }
+
+    private void dandara() {
+        System.out.println("aespa loona");
+        System.out.println("Estou colaborando com o git");
+    }
 }
+
